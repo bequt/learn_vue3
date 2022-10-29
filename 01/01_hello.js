b684d80f867d010d01431104b9625358567ac04a
bequt <bequth@gmail.com>
2022-10-29 09:55:26 +0800

    笔记记录

--- 01/01_hello.js
+++ 01/01_hello.js
@@ -1,31 +1,11 @@
 class Person {
-    #name // 实例使用#开头就变成私有属性
-    #age
-
-    constructor(name, age) {
-        this.#name = name // 赋值也要加#
-        this.#age = age
-    }
+    name = "孙悟空"
+    age = 18
 
     sayHello() {
-        console.log(this.#name)
-    }
-
-    setName(name) {
-        this.#name = name
-    }
-
-    getName() {
-        return this.#name
-    }
-
-    get age() {
-        return this.#age
+        console.log("1111")
     }
 }
 
-const p1 = new Person("张三", 18)
-p1.sayHello()
-p1.setName("李四")
-console.log(p1.getName())
-console.log(p1.getName())
+const p = new Person()
+console.log(p.constructor)
